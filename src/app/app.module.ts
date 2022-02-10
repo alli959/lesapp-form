4a4d7d9aab6ef1191d558f8caf859344cdcfed85
alli959 <alexandergudmundsson@gmail.com>
2022-02-10 03:58:21 +0000

    client changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { RoutingModule  } from 'angular-routing';
+import { AmplifyAuthenticatorModule } from '@aws-amplify/ui-angular';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -10,6 +13,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     FormsModule,
     BrowserModule,
+    RoutingModule,
+    AmplifyAuthenticatorModule,
     HttpClientModule
   ],
   providers: [],
